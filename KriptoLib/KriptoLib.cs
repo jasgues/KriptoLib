af1c16a77c5c67fb39ce413f54e3a3dad196121c
jasgues <webmanyak@gmail.com>
2022-09-09 00:17:39 +0300

    1.0.0.1
    1.0.0.1

---
+++ KriptoLib/KriptoLib.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KriptoLib
+{
+    public class KriptoLib
+    {   
+        ///
+        public static byte[] ByteDonustur(string deger)
+        {
+
+            UnicodeEncoding ByteConverter = new UnicodeEncoding();
+            return ByteConverter.GetBytes(deger);
+
+        }
+
+        public static byte[] Byte8(string deger)
+        {
+            char[] arrayChar = deger.ToCharArray();
+            byte[] arrayByte = new byte[arrayChar.Length];
+            for (int i = 0; i < arrayByte.Length; i++)
+            {
+                arrayByte[i] = Convert.ToByte(arrayChar[i]);
+            }
+            return arrayByte;
+        }
+        ///
+
+        public string MD5(string strGiris)
+        {
+            if (strGiris == "" || strGiris == null)
+            {
+                throw new ArgumentNullException("Şifrelenecek veri yok");
+            }
+            else
+            {
+                MD5CryptoServiceProvider sifre = new MD5CryptoServiceProvider();
+                byte[] arySifre = ByteDonustur(strGiris);
+                byte[] aryHash = sifre.ComputeHash(arySifre);
+                return BitConverter.ToString(aryHash);
+            }
+        }
+
+        public string SHA1(string strGiris)
+        {
+            if (strGiris == "" || strGiris == null)
+            {
+                throw new ArgumentNullException("Şifrelenecek veri yok.");
+            }
+            else
+            {
+                SHA1CryptoServiceProvider sifre = new SHA1CryptoServiceProvider();
+                byte[] arySifre = ByteDonustur(strGiris);
+                byte[] aryHash = sifre.ComputeHash(arySifre);
+                return BitConverter.ToString(aryHash);
+            }
+        }
+
+        public string SHA256(string strGiris)
+        {
+            if (strGiris == "" || strGiris == null)
+            {
+                throw new ArgumentNullException("Şifrelenecek Veri Yok");
+            }
+            else
+            {
+                SHA256Managed sifre = new SHA256Managed();
+                byte[] arySifre = ByteDonustur(strGiris);
+                byte[] aryHash = sifre.ComputeHash(arySifre);
+                return BitConverter.ToString(aryHash);
+            }
+        }
+
+
+        public string SHA384(string strGiris)
+        {
+            if (strGiris == "" || strGiris == null)
+            {
+                throw new ArgumentNullException("Şifrelenecek veri bulunamadı.");
+            }
+            else
+            {
+                SHA384Managed sifre = new SHA384Managed();
+                byte[] arySifre = ByteDonustur(strGiris);
+                byte[] aryHash = sifre.ComputeHash(arySifre);
+                return BitConverter.ToString(aryHash);
+            }
+        }
+
+        public string SHA512(string strGiris)
+        {
+            if (strGiris == "" || strGiris == null)
+            {
+                throw new ArgumentNullException("Şifrelenecek veri yok.");
+            }
+            else
+            {
+                SHA512Managed sifre = new SHA512Managed();
+                byte[] arySifre = ByteDonustur(strGiris);
+                byte[] aryHash = sifre.ComputeHash(arySifre);
+                return BitConverter.ToString(aryHash);
+            }
+        }
+
+        public string DESSifrele(string strGiris)
+        {
+            string sonuc = "";
+            if (strGiris == "" || strGiris == null)
+            {
+                throw new ArgumentNullException("Şifrelenecek veri yok");
+            }
+            else
+            {
+                byte[] aryKey = Byte8("12345678"); // BURAYA 8 bit string DEĞER GİRİN
+                byte[] aryIV = Byte8("12345678"); // BURAYA 8 bit string DEĞER GİRİN
+                DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+                MemoryStream ms = new MemoryStream();
+                CryptoStream cs = new CryptoStream(ms, cryptoProvider.CreateEncryptor(aryKey, aryIV), CryptoStreamMode.Write);
+                StreamWriter writer = new StreamWriter(cs);
+                writer.Write(strGiris);
+                writer.Flush();
+                cs.FlushFinalBlock();
+                writer.Flush();
+                sonuc = Convert.ToBase64String(ms.GetBuffer(), 0, (int)ms.Length);
+                writer.Dispose();
+                cs.Dispose();
+                ms.Dispose();
+            }
+            return sonuc;
+        }
+
+        public string DESCoz(string strGiris)
+        {
+            string strSonuc = "";
+            if (strGiris == "" || strGiris == null)
+            {
+                throw new ArgumentNullException("Şifrelenecek veri yok.");
+            }
+            else
+            {
+                byte[] aryKey = Byte8("12345678");
+                byte[] aryIV = Byte8("12345678");
+                DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+                MemoryStream ms = new MemoryStream(Convert.FromBase64String(strGiris));
+                CryptoStream cs = new CryptoStream(ms, cryptoProvider.CreateDecryptor(aryKey, aryIV), CryptoStreamMode.Read);
+                StreamReader reader = new StreamReader(cs);
+                strSonuc = reader.ReadToEnd();
+                reader.Dispose();
+                cs.Dispose();
+                ms.Dispose();
+            }
+            return strSonuc;
+        }
+        //
+        public string RC2Sifrele(string strGiris)
+        {
+            string sonuc = "";
+            if (strGiris == "" || strGiris == null)
+            {
+                throw new ArgumentNullException("Şifrelenecek veri yok.");
+            }
+            else
+            {
+                byte[] aryKey = Byte8("12345678");
+                byte[] aryIV = Byte8("12345678");
+                RC2CryptoServiceProvider dec = new RC2CryptoServiceProvider();
+                MemoryStream ms = new MemoryStream();
+                CryptoStream cs = new CryptoStream(ms, dec.CreateEncryptor(aryKey, aryIV), CryptoStreamMode.Write);
+                StreamWriter writer = new StreamWriter(cs);
+                writer.Write(strGiris);
+                writer.Flush();
+                cs.FlushFinalBlock();
+                writer.Flush();
+                sonuc = Convert.ToBase64String(ms.GetBuffer(), 0, (int)ms.Length);
+                writer.Dispose();
+                cs.Dispose();
+                ms.Dispose();
+            }
+            return sonuc;
+        }
+
+        public string RC2Coz(string strGiris)
+        {
+            string strSonuc = "";
+            if (strGiris == "" || strGiris == null)
+            {
+                throw new ArgumentNullException("Şifresi çözülecek veri yok.");
+            }
+            else
+            {
+                byte[] aryKey = Byte8("12345678");
+                byte[] aryIV = Byte8("12345678");
+                RC2CryptoServiceProvider cp = new RC2CryptoServiceProvider();
+                MemoryStream ms = new MemoryStream(Convert.FromBase64String(strGiris));
+                CryptoStream cs = new CryptoStream(ms, cp.CreateDecryptor(aryKey, aryIV), CryptoStreamMode.Read);
+                StreamReader reader = new StreamReader(cs);
+                strSonuc = reader.ReadToEnd();
+                reader.Dispose();
+                cs.Dispose();
+                ms.Dispose();
+            }
+            return strSonuc;
+        }
+        //
+        public string RijndaelSifrele(string strGiris)
+        {
+            string sonuc = "";
+            if (strGiris == "" || strGiris == null)
+            {
+                throw new ArgumentNullException("Şifrelenecek veri yok.");
+            }
+            else
+            {
+                byte[] aryKey = Byte8("12345678");
+                byte[] aryIV = Byte8("1234567812345678");
+                RijndaelManaged dec = new RijndaelManaged();
+                dec.Mode = CipherMode.CBC;
+                MemoryStream ms = new MemoryStream();
+                CryptoStream cs = new CryptoStream(ms, dec.CreateEncryptor(aryKey, aryIV), CryptoStreamMode.Write);
+                StreamWriter writer = new StreamWriter(cs);
+                writer.Write(strGiris);
+                writer.Flush();
+                cs.FlushFinalBlock();
+                writer.Flush();
+                sonuc = Convert.ToBase64String(ms.GetBuffer(), 0, (int)ms.Length);
+                writer.Dispose();
+                cs.Dispose();
+                ms.Dispose();
+            }
+            return sonuc;
+        }
+        public string RijndaelCoz(string strGiris)
+        {
+            string strSonuc = "";
+            if (strGiris == "" || strGiris == null)
+            {
+                throw new ArgumentNullException("Şifrezi çözülecek veri yok.");
+            }
+            else
+            {
+                byte[] aryKey = Byte8("12345678");
+                byte[] aryIV = Byte8("1234567812345678");
+                RijndaelManaged cp = new RijndaelManaged();
+                MemoryStream ms = new MemoryStream(Convert.FromBase64String(strGiris));
+                CryptoStream cs = new CryptoStream(ms, cp.CreateDecryptor(aryKey, aryIV), CryptoStreamMode.Read);
+                StreamReader reader = new StreamReader(cs);
+                strSonuc = reader.ReadToEnd();
+                reader.Dispose();
+                cs.Dispose();
+                ms.Dispose();
+            }
+            return strSonuc;
+        }
+
+
+
+        public string RSASifrele(string strGiris, out RSAParameters prm)
+        {
+            string strSonuc = "";
+            if (strGiris == "")
+            {
+                throw new ArgumentNullException("Şifrelenecek veri yok.");
+            }
+            else
+            {
+                byte[] aryDizi = ByteDonustur(strGiris);
+                RSACryptoServiceProvider dec = new RSACryptoServiceProvider();
+                prm = dec.ExportParameters(true);
+                byte[] aryDonus = dec.Encrypt(aryDizi, false);
+                strSonuc = Convert.ToBase64String(aryDonus);
+            }
+            return strSonuc;
+        }
+
+        public string RSACoz(string strGiris, RSAParameters prm)
+        {
+            string strSonuc = "";
+            if (strGiris == "" || strGiris == null)
+            {
+                throw new ArgumentNullException("Çözülecek kayıt yok");
+            }
+            else
+            {
+                RSACryptoServiceProvider dec = new RSACryptoServiceProvider();
+                byte[] aryDizi = Convert.FromBase64String(strGiris);
+                UnicodeEncoding UE = new UnicodeEncoding();
+                dec.ImportParameters(prm);
+                byte[] aryDonus = dec.Decrypt(aryDizi, false);
+                strSonuc = UE.GetString(aryDonus);
+            }
+            return strSonuc;
+        }
+        public string Sezar5Sifrele(string str)
+        {
+            string cikti = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                char c = str[i];
+                for (int j = 0; j < 6; j++)
+                {
+                    c++;
+                }
+                cikti += c;
+            }
+
+            return cikti;
+        }
+
+        public string Sezar5Coz(string str)
+        {
+            string cikti = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                char c = str[i];
+                for (int j = 0; j < 6; j++)
+                {
+                    c--;
+                }
+                cikti += c;
+            }
+
+            return cikti;
+        }
+
+
+
+
+
+
+    }
+}
